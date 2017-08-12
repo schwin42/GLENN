ce077df250ea03ae91362da3240ae409c77a3b32
Schwin <schwin42@Flan.local>
2017-08-12 16:40:44 -0700

    Added Frankenstein module

---
+++ Python/ScratchPad.py
@@ -0,0 +1,14 @@
+import gym
+
+env = gym.make('SpaceInvaders-v0')
+# env = gym.make('Asteroids-v0')
+env.reset()
+
+
+for i in range(100000):
+	env.render()
+	observation, reward, done, info = env.step(env.action_space.sample())
+	print("info: ", info)
+	lives = info['ale.lives']
+	print("lives: ", lives)
+	print("action space", env.action_space)
