0c2b5b6c9c7e33e7b939d58e7da13f0a56c167c5
Schwin <schwin42@Flan.local>
2017-09-23 17:46:25 -0700

    First pass at forward propagation

--- Python/Main.py
+++ Python/Main.py
@@ -1,6 +1,7 @@
 import gym
 import numpy as np
 import tensorflow as tf
+import tensorflow.contrib.slim as slim
 
 
 print("program started")
@@ -8,45 +9,86 @@ print("program started")
 class Agent() :
 	def __init__ (self, learning_rate, nodes_per_layer, hidden_layer_count):
 		#Create placeholders and operations
-		self.chosen_action = "" #Controller output
+		self.state = tf.placeholder(shape = [None, INPUT_SIZE], dtype = tf.float32)
+		xavier_init = tf.contrib.layers.xavier_initializer()
+		#print("state rank" + str(tf.rank(state)))
+		#print("state shape" + str(tf.shape(state)))
+
+		#Advantage network
+		self._advantage_fc_layers = []
+		_layer_input = self.state
+		for i in range(hidden_layer_count):
+			layer = slim.fully_connected(_layer_input, nodes_per_layer, weights_initializer = xavier_init, biases_initializer = xavier_init, activation_fn = tf.nn.relu) 
+			self._advantage_fc_layers.append(layer)
+			_layer_input = layer
+		
+		#Value network
+		# self.value_fc_layers = []
+		# _layer_input = state
+		# for i in range(hidden_layer_count):
+		# 	layer = slim.fully_connected(state, nodes_per_layer, weights_initializer = xavier_init, bias_initializer = tf.xavier_init, activation_fn = tf.nn.relu) 
+		# 	self.value_fc_layers.append(layer)
+		# 	layer_input = layer
+
+		#Output
+		self.advantage_prediction = slim.fully_connected(self._advantage_fc_layers[-1], env.action_space.n, weights_initializer = xavier_init, biases_initializer = xavier_init, activation_fn = tf.nn.softmax) #Should this really be softmax or should this represent expected reward?
+		#self value_output_layer = slim.fully_connected(value_fc_layers[-1], 1, weights_initializer = xavier_init, bias_initializer = tf.xavier_init)
+		self.chosen_action = tf.argmax(self.advantage_prediction, 1)[0]
 
 #Constants
 epoch_count = 50
 backprop_frequency = 5
 max_epoch_length = 200
 
+#Environment constants
+INPUT_SIZE = 4
+
 #Initialize environment
 env = gym.make("CartPole-v0")
 
-total_reward_by_episode = []
-for i in range(epoch_count):
-#For each episode
-	#print("Starting episode: " + str(i))
-	running_reward = 0.
-	env.reset()
+#Initalize agent
+agent = Agent(0.0001, 128, 2)
+init = tf.global_variables_initializer()
 
-	#For each step
-	for j in range(max_epoch_length):
-		#print("Beginning of step: " + str(j))
-		env.render()
-		#Forward feed input features through fully connected hidden layers
-	
-		#Choose between network output and random action
+with tf.Session() as sess:
+	sess.run(init)
+	total_reward_by_episode = []
+	for i in range(epoch_count):
+	#For each episode
+		#print("Starting episode: " + str(i))
+		running_reward = 0.
+		state = env.reset()
+		is_done = False
 
-		observation, reward, done, info = env.step(env.action_space.sample()) #Choose random action
-		running_reward += reward
+		#For each step
+		for j in range(max_epoch_length):
+			#print("Beginning of step: " + str(j))
+			env.render()
+		
+			#Forward feed input features through fully connected hidden layers
+			#agent.advantage
+			action = sess.run(agent.chosen_action, feed_dict = {agent.state: [state]})
 
-		if done == True:
-			total_reward_by_episode.append(running_reward)
-			print("Reward for episode " + str(i) + ": " + str(running_reward))
-			if(i != 0 and i % 10 == 0):
-				print("Mean reward for last ten episodes: " + str(np.mean(total_reward_by_episode)))
-			break
+			#TODO Choose between network output and random action
+			#print("ad prob" + str(advantage_probabilities))
+			
+			print("action: " + str(action))
+			
+			observation, reward, is_done, info = env.step(action)
+			#Choose random action - observation, reward, done, info = env.step(env.action_space.sample()) #Choose random action
+			running_reward += reward
+
+			if is_done == True:
+				total_reward_by_episode.append(running_reward)
+				print("Reward for episode " + str(i) + ": " + str(running_reward))
+				if(i != 0 and i % 10 == 0):
+					print("Mean reward for last ten episodes: " + str(np.mean(total_reward_by_episode)))
+				break
 
 
-	#if i != 0 and i % backprop_frequency == 0:
-		#print("backprop")
-		#Backpropagate reward
+		#if i != 0 and i % backprop_frequency == 0:
+			#print("backprop")
+			#Backpropagate reward
 
 
 
