bc4adeca9b0ff36c96f2b7985ad8bcaae11dccf4
Paul Schwin <schwin42@gmail.com>
2017-09-27 13:42:14 -0700

    Successful test of experience buffer

--- Python/UnitTests.py
+++ Python/UnitTests.py
@@ -7,48 +7,38 @@ import unittest
 import numpy as np
 from Model import ExperienceBuffer
 
-class TestStringMethods(unittest.TestCase):
-
-	def test_upper(self):
-		self.assertEqual('foo'.upper(), 'FOO')
-
-	def test_isupper(self):
-		self.assertTrue('FOO'.isupper())
-		self.assertFalse('Foo'.isupper())
-
-	def test_split(self):
-		s = 'hello world'
-		self.assertEqual(s.split(), ['hello', 'world'])
-		# check that s.split fails when the separator is not a string
-		with self.assertRaises(TypeError):
-			s.split(2)
-
 class TestExperienceBuffer(unittest.TestCase):
-	def test_sample(self):
-		buffer = ExperienceBuffer()
-		experience = []
-		for i in range(10):
-			observation = [[i, i, i, i], i, i, [i, i, i, i], i]
-			experience.append(observation)
-		
-		buffer.add(experience)
-		exhaustive_sample = buffer.sample(10)
-		
-		for i in range(10):
-			match_found = False
-			for j in range(len(exhaustive_sample)):
-				if experience[i][0] == exhaustive_sample[j][0]:
-					np.delete(exhaustive_sample, j)
-					match_found = True
-					break
-				else:
-					continue
-				
-			#Inner loop ended with no matching experience found, so fail
-			self.assertTrue(match_found, "Experience match not found in sample")
-		
-		self.assertEqual(len(exhaustive_sample), 0, "Buffer contains more items than source experience")	
-	
+    def test_sample(self):
+        buffer = ExperienceBuffer()
+        experience = []
+        for i in range(10):
+            observation = [[i, i, i, i], i, i, [i, i, i, i], i]
+            experience.append(observation)
+        
+        buffer.add(experience)
+        exhaustive_sample = buffer.sample(10)
+        
+        for i in range(10):
+            match_found = False
+            for j in range(len(exhaustive_sample)):
+                if experience[i][0] == exhaustive_sample[j][0]:
+                    #print("exhaustive sample: " + str(exhaustive_sample))
+                    #print("j = " + str(j))
+                    #print("item b: " + str(exhaustive_sample[j]))
+                    exhaustive_sample = np.delete(exhaustive_sample, j, 0)
+                    #print("item a: " + str(exhaustive_sample[j]))
+                    match_found = True
+                    break
+                else:
+                    continue
+                
+            #Inner loop ended with no matching experience found, so fail
+            self.assertTrue(match_found, "Experience match not found in sample")
+            if not match_found: 
+                return None
+        
+        self.assertEqual(len(exhaustive_sample), 0, "Sample was not successfully emptied with matches from experience")    
+    
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
